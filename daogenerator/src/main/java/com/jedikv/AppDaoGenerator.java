34602bcabff5896d7e1aefc89ff7bef17addb8b2
Kurian Vithayathil <jedikv+github@gmail.com>
2015-05-02 16:29:50 +0100

    Added DaoGenerator program to generate entity classes for currency

---
+++ daogenerator/src/main/java/com/jedikv/AppDaoGenerator.java
@@ -0,0 +1,38 @@
+package com.jedikv;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class AppDaoGenerator {
+
+    public static void main (String [] args) throws Exception {
+
+        Schema schema = new Schema(1, "converter_db");
+        schema.enableKeepSectionsByDefault();
+
+        createCurrencyTable(schema);
+        createCurrencyPairTable(schema);
+
+        new DaoGenerator().generateAll(schema, args[0]);
+    }
+
+    public static void createCurrencyTable(Schema schema) {
+
+        Entity entity = schema.addEntity("CurrencyEntity");
+        entity.addStringProperty("symbol");
+        entity.addStringProperty("name");
+        entity.addStringProperty("country_name");
+        entity.addLongProperty("numeric_code").primaryKey();
+        entity.addStringProperty("currency_code").unique().notNull();
+    }
+
+    public static void createCurrencyPairTable(Schema schema) {
+
+        Entity entity = schema.addEntity("CurrencyPairEntity");
+        entity.addStringProperty("pair").unique().notNull();
+        entity.addIdProperty();
+        entity.addDateProperty("date");
+        entity.addIntProperty("rate");
+    }
+}
