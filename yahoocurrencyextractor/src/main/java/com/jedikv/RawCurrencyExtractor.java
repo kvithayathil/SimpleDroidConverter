b6dc263018abd82e1c894ac710fa790838d6385e
Kurian Vithayathil <jedikv+github@gmail.com>
2015-05-02 17:37:29 +0100

    Renamed currency extractor main class

--- yahoocurrencyextractor/src/main/java/com/jedikv/RawCurrencyExtractor.java
+++ yahoocurrencyextractor/src/main/java/com/jedikv/RawCurrencyExtractor.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class YahooCurrencyExtractor {
+public class RawCurrencyExtractor {
 
     private static String outputPath;
 
